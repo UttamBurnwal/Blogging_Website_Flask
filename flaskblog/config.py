c72e35aa42677389248d6f63e51b7c47d5fd2c29
uttam burnwal <uttamburnwal56@gmail.com>
2022-02-23 16:09:04 +0530

    Flask Source Code

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'ae1d39e7ed61927b1e32dd690b274ebb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
